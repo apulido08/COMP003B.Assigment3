c88608994d2b66f275d16ec6337b1ce93af98c12
Alvaro Pulido <alavropulido@my.whccd.edu>
2025-04-02 22:50:23 -0700

    added numerous updates to files

---
+++ COMP003B.Assigment3/Controllers/EventController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assigment3.Models;
+
+namespace COMP003B.Assigment3.Controllers
+{
+    public class EventController : Controller
+    {
+        [HttpGet("event/register/{eventCode}")]
+
+        public IActionResult Register(string eventCode)
+        {
+           
+            return View();
+        }
+
+        [HttpGet("register")]
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromForm] EventRegistration registration)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(registration);
+            }
+            return RedirectToAction("Success", registration);
+        }
+        [Route("success")]
+
+        public IActionResult Success(EventRegistration registration)
+        {
+            return View(registration);
+        }
+
+        
+    }
+}
---
+++ COMP003B.Assigment3/Models/EventRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assigment3.Models
+{
+    public class EventRegistration : Controller
+    {
+            [Required]
+            public string FullName { get; set; }
+            [MinLength(3)]
+
+            [Required]
+            public string Email { get; set; }
+
+            [Required]
+            public string EventCode { get; set; }
+            [MaxLength(20)]
+
+            [Required]
+            public int Tickets { get; set; }
+            [Range(1, 10)]
+
+            public string ReferralCode { get; set; }
+        }
+    }
+
